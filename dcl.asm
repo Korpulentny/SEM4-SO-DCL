84d365272709a1be61260786aa8577ee26a32399
Tomasz Patyna <tp406285@students.mimuw.edu.pl>
2020-03-29 21:30:34 +0200

    DZIALAJACE NIEZOPTYMALIZOWANE

--- dcl.asm
+++ dcl.asm
@@ -6,6 +6,9 @@ STDOUT equ 1
 PERMUTATION_BUFFER_SIZE equ 43
 ALPHABET_SIZE equ 42
 INPUT_BUFFER_SIZE equ 4096
+ROTOR_POSITION_1 equ 27
+ROTOR_POSITION_2 equ 33
+ROTOR_POSITION_3 equ 35
 
 section .data
 
@@ -32,6 +35,9 @@ section .bss
   permLT resb ALPHABET_SIZE
   permRT resb ALPHABET_SIZE
   inputBuffer resb INPUT_BUFFER_SIZE
+  identity resb ALPHABET_SIZE*3
+  leftKey resb 1
+  rightKey resb 1
 
 section .text
 
@@ -68,7 +74,32 @@ _start:
   cmp r10, 33                           ;sprawdzamy czy '1' wystąpiło w każdej permutacji dokładnie raz
   jne _exit1
   checkChar BYTE [rdx]
-  checkChar BYTE [rdx+1]
+  checkChar BYTE [rdx + 1]
+  xor r10, r10
+  mov r10b, BYTE [rdx]
+  mov BYTE [leftKey], r10b
+  mov r10b, BYTE [rdx + 1]
+  mov BYTE [rightKey], r10b
+  sub BYTE [leftKey], 49
+  sub BYTE [rightKey], 49
+  xor rcx, rcx
+
+_fillIdentityLoop:
+  mov BYTE [identity + rcx], cl
+  mov BYTE [identity + ALPHABET_SIZE + rcx], cl
+  mov BYTE [identity + 2 * ALPHABET_SIZE + rcx], cl
+  inc cl
+  cmp rcx, ALPHABET_SIZE
+
+  jne _fillIdentityLoop
+
+
+
+
+
+
+
+
 
 ;  1.Wczytuje na bufor i sprawdzam czy rax!=0 bo jak nie jest różny to exit
 ;  2.Mam jakis counter ktory zwiekszam z każdą literką
@@ -87,19 +118,104 @@ _readInput:
   syscall
   cmp rax, 0                           ;jeśli nie wczytaliśmy nic, to kończymy
   je _exit0
-  mov r10, rax                         ;r10 będzie przechowywało liczbę ostatnio wczytanych znaków
-  dec r10
+  mov r9, rax                         ;r9 będzie przechowywało liczbę ostatnio wczytanych znaków
+;  dec r9
   xor rcx, rcx                         ;przygotowuję rcx pod licznik wczytanych znaków
 _processInputCharLoop:
   checkChar BYTE [inputBuffer + rcx]
   ;tutaj wleci szyfrowanko pyk pyk bedzie super, nie wiem jak to zrobic
+
+  inc BYTE [rightKey]
+
+  cmp BYTE [rightKey], ALPHABET_SIZE
+  jne _checkRotorPositions
+  xor r11, r11
+  mov BYTE [rightKey], r11b
+  jmp _beginCypher
+_checkRotorPositions:
+  cmp BYTE [rightKey], ROTOR_POSITION_1
+  je _incrementingLeft
+  cmp BYTE [rightKey], ROTOR_POSITION_2
+  je _incrementingLeft
+  cmp BYTE [rightKey], ROTOR_POSITION_3
+  je _incrementingLeft
+
+  jmp _beginCypher
+_incrementingLeft:
+  inc BYTE [leftKey]
+  cmp BYTE [leftKey], ALPHABET_SIZE
+  jne _beginCypher
+  xor r11, r11
+  mov BYTE [leftKey], r11b
+_beginCypher:
+
+  xor r8, r8
+  mov r8b, BYTE [inputBuffer + rcx]
+  sub r8b, 49
+
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  add al, BYTE [rightKey]
+  mov r8b, BYTE [identity + rax]
+
+  mov r8b, BYTE [permR + r8]
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  sub al, BYTE [rightKey]
+  mov r8b, BYTE [identity + rax]
+
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  add al, BYTE [leftKey]
+  mov r8b, BYTE [identity + rax]
+
+  mov r8b, BYTE [permL + r8]
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  sub al, BYTE [leftKey]
+  mov r8b, BYTE [identity + rax]
+
+  mov r8b, BYTE [permT + r8]
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  add al, BYTE [leftKey]
+  mov r8b, BYTE [identity + rax]
+
+  mov r8b, BYTE [permLT + r8]
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  sub al, BYTE [leftKey]
+  mov r8b, BYTE [identity + rax]
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  add al, BYTE [rightKey]
+  mov r8b, BYTE [identity + rax]
+
+  mov r8b, BYTE [permRT + r8]
+
+  mov rax, ALPHABET_SIZE
+  add rax, r8
+  sub al, BYTE [rightKey]
+  mov r8b, BYTE [identity + rax]
+
+  add r8b, 49
+  mov BYTE [inputBuffer + rcx], r8b
+
   inc rcx
-  cmp rcx, r10
+  cmp rcx, r9
   jne _processInputCharLoop
+
   mov rax, SYS_WRITE
   mov rdi, STDOUT
   mov rsi, inputBuffer
-  mov rdx, r10
+  mov rdx, r9
   syscall
 
   jmp _readInput
