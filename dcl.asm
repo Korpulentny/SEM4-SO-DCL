9d92a71b25b54da570e83e345e05f529fd4e8d2c
Tomasz Patyna <tp406285@students.mimuw.edu.pl>
2020-03-30 00:02:50 +0200

    784, al ena pewno dziala

--- dcl.asm
+++ dcl.asm
@@ -12,11 +12,19 @@ ROTOR_POSITION_3 equ 35
 
 section .data
 
-%macro checkChar 1                  ;sprawdzamy czy argument jest pomiędzy '1' a 'Z' w ascii
-  cmp         %1, 49
+%macro checkChar 1                  ;sprawdzamy czy argument jest pomiędzy 1 a Z w ascii
+  mov         al, %1
+  cmp         al, 49
   jl          _exit1
   cmp         al, 90
   jg          _exit1
+%endmacro
+
+
+%macro exit 1
+  mov         rax, SYS_EXIT
+  mov         rdi, %1
+  syscall
 %endmacro
 
 section .bss
@@ -26,10 +34,15 @@ permR resb ALPHABET_SIZE
 permT resb ALPHABET_SIZE
 permLT resb ALPHABET_SIZE
 permRT resb ALPHABET_SIZE
-inputBuffer resb INPUT_BUFFER_SIZE
+  inputBuffer resb INPUT_BUFFER_SIZE
 identity resb ALPHABET_SIZE*3
+leftKey resb 1
+rightKey resb 1
 
 section .text
+
+
+
 
 global _start
 
@@ -59,14 +72,21 @@ _start:
   jne         _exit1
   cmp         r10d, 33              ;sprawdzamy czy '1' wystąpiło w każdej permutacji dokładnie raz
   jne         _exit1
+
   checkChar   BYTE [rdx]
   checkChar   BYTE [rdx + 1]
-  xor         r10d, r10d
-  mov         r12b, BYTE [rdx]
-  mov         r13b, BYTE [rdx + 1]
 
-  sub         r12b, 49
-  sub         r13b, 49
+  xor         r10, r10
+  mov         r10b, BYTE [rdx]
+
+  mov         BYTE [leftKey], r10b
+
+  mov         r10b, BYTE [rdx + 1]
+
+  mov         BYTE [rightKey], r10b
+  sub         BYTE [leftKey], 49
+  sub         BYTE [rightKey], 49
+
   xor         rcx, rcx
 
 _fillIdentityLoop:
@@ -78,6 +98,23 @@ _fillIdentityLoop:
 
   jne         _fillIdentityLoop
 
+
+
+
+
+
+
+
+
+; 1.Wczytuje na bufor i sprawdzam czy rax!=0 bo jak nie jest różny to exit
+; 2.Mam jakis counter ktory zwiekszam z każdą literką
+; 3.Wczytuje literkę, sprawdzam czy jest z dopuszczalnego przedzialu, bo jak nie to elo nara exit
+; 4.Jak jest z dopuszczalnego przedzialu to super pusczam na niej szyfrowanie, zaszfrowaną literką nadpisuje pierwotną w buforze
+; 5.inkrementuje counter, sprawdzam czy przeorałem już tyle literek co wczytałem bajtów, jesli nie to ide do 3.
+; 6.jesli przeorałem już wszystkie literki to wypisuje bufor i moge cofnąć się do punktu 1.
+
+
+
 _readInput:
   mov         rax, SYS_READ
   mov         rdi, STDIN
@@ -87,32 +124,34 @@ _readInput:
   cmp         rax, 0                ;jeśli nie wczytaliśmy nic, to kończymy
   je          _exit0
   mov         r9, rax               ;r9 będzie przechowywało liczbę ostatnio wczytanych znaków
+; dec r9
   xor         rcx, rcx              ;przygotowuję rcx pod licznik wczytanych znaków
 _processInputCharLoop:
   checkChar   BYTE [inputBuffer + rcx]
-  inc         r13b
+;tutaj wleci szyfrowanko pyk pyk bedzie super, nie wiem jak to zrobic
 
-  cmp         r13b, ALPHABET_SIZE
+  inc         BYTE [rightKey]
+
+  cmp         BYTE [rightKey], ALPHABET_SIZE
   jne         _checkRotorPositions
-  xor         r11b, r11b
-  mov         r13b, r11b
+  xor         r11, r11
+  mov         BYTE [rightKey], r11b
   jmp         _beginCypher
 _checkRotorPositions:
-  cmp         r13b, ROTOR_POSITION_1
+  cmp         BYTE [rightKey], ROTOR_POSITION_1
   je          _incrementingLeft
-  cmp         r13b, ROTOR_POSITION_2
+  cmp         BYTE [rightKey], ROTOR_POSITION_2
   je          _incrementingLeft
-  cmp         r13b, ROTOR_POSITION_3
+  cmp         BYTE [rightKey], ROTOR_POSITION_3
   je          _incrementingLeft
 
   jmp         _beginCypher
 _incrementingLeft:
-
-  inc         r12b
-  cmp         r12b, ALPHABET_SIZE
+  inc         BYTE [leftKey]
+  cmp         BYTE [leftKey], ALPHABET_SIZE
   jne         _beginCypher
-  xor         r11b, r11b
-  mov         r12b, r11b
+  xor         r11, r11
+  mov         BYTE [leftKey], r11b
 _beginCypher:
 
   xor         r8, r8
@@ -120,47 +159,55 @@ _beginCypher:
   sub         r8b, 49
 
 
-  mov         al, ALPHABET_SIZE
-  add         al, r8b
-  add         al, r13b
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  add         al, BYTE [rightKey]
   mov         r8b, BYTE [identity + rax]
 
   mov         r8b, BYTE [permR + r8]
 
-  mov         al, ALPHABET_SIZE
-  add         al, r8b
-  sub         al, r13b
-  add         al, r12b
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  sub         al, BYTE [rightKey]
+  mov         r8b, BYTE [identity + rax]
 
+
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  add         al, BYTE [leftKey]
   mov         r8b, BYTE [identity + rax]
 
   mov         r8b, BYTE [permL + r8]
 
-  mov         al, ALPHABET_SIZE
-  add         al, r8b
-  sub         al, r12b
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  sub         al, BYTE [leftKey]
   mov         r8b, BYTE [identity + rax]
 
   mov         r8b, BYTE [permT + r8]
 
-  mov         al, ALPHABET_SIZE
-  add         al, r8b
-  add         al, r12b
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  add         al, BYTE [leftKey]
   mov         r8b, BYTE [identity + rax]
 
   mov         r8b, BYTE [permLT + r8]
 
-  mov         al, ALPHABET_SIZE
-  add         al, r8b
-  sub         al, r12b
-  add         al, r13b
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  sub         al, BYTE [leftKey]
+  mov         r8b, BYTE [identity + rax]
+
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  add         al, BYTE [rightKey]
   mov         r8b, BYTE [identity + rax]
 
   mov         r8b, BYTE [permRT + r8]
 
-  mov         al, ALPHABET_SIZE
-  add         al, r8b
-  sub         al, r13b
+  mov         rax, ALPHABET_SIZE
+  add         rax, r8
+  sub         al, BYTE [rightKey]
   mov         r8b, BYTE [identity + rax]
 
   add         r8b, 49
@@ -230,3 +277,5 @@ _exit1:
   mov         rdi, 1
   syscall
 
+;./dcl 'G=EN9LFW;VBMHA7:ZTY8K@QI6X>OJ2PR?4DSCU1<53' '4O8?T3FBKX7=G96;ZPQUDCJV:>LEY1HRS5@<MNWA2I' 'XSNEG9;J6Z7B>=CPD<?A4Q5KY8HTO3M@FV2LWRU1I:' 'P='
+;9=O4M9YS<:4SJ2NS3BHTWLZOA
